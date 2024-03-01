95dc1a6e51a77dbe4b053a39b4996e04f83a1cc9
TheGreatCactus <mikolajsz31@gmail.com>
2024-03-01 21:11:08 +0100

    Initial commit

---
+++ src/main/kotlin/net/mikolaj/firefliesmod/FirefliesModClient.kt
@@ -0,0 +1,9 @@
+package net.mikolaj.firefliesmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object FirefliesModClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
---
+++ src/main/kotlin/net/mikolaj/firefliesmod/FirefliesModDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.mikolaj.firefliesmod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object FirefliesModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
