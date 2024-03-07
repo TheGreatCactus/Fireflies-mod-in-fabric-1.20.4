9644211140dedb477f1c17f9fbf14897f094ec97
TheGreatCactus <mikolajsz31@gmail.com>
2024-03-07 20:03:10 +0100

    something

--- src/main/kotlin/net/mikolaj/firefliesmod/FirefliesModClient.kt
+++ src/main/kotlin/net/mikolaj/firefliesmod/FirefliesModClient.kt
@@ -1,9 +1,14 @@
 package net.mikolaj.firefliesmod
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry
+import net.mikolaj.firefliesmod.entity.custom.BottledFirefliesEntity
+import net.mikolaj.firefliesmod.mixin.ModEntities
+import net.minecraft.client.render.entity.EntityRendererFactory
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer
 
 object FirefliesModClient : ClientModInitializer {
     override fun onInitializeClient() {
-
+        EntityRendererRegistry.register(ModEntities.BOTTLED_FIREFLIES_ENTITY, FlyingItemEntityRenderer::class.java.newInstance() as EntityRendererFactory<BottledFirefliesEntity>)
     }
 }
